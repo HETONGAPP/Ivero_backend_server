6b52f05b99376dc19bd9c261472189de7b0c7368
HETONGAPP <44877369+HETONGAPP@users.noreply.github.com>
2023-07-05 13:47:37 -0400

    Update webrtc_install.sh

--- webrtc_install.sh
+++ webrtc_install.sh
@@ -55,7 +55,7 @@ sudo apt install software-properties-common
 sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt upgrade
+sudo apt-get update && sudo apt upgrade
 sudo apt install ros-humble-desktop python3-argcomplete ros-dev-tools
 echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc 
 sudo apt install ros-humble-diagnostic-updater &&  sudo apt install ros-humble-image-transport-plugins && sudo apt install ros-humble-rosbridge-server
